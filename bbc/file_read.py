d2584c20ce4094d1c4e7d13a0c296f80dd2f22dc
prakashnitie14 <137127820+prakashnitie14@users.noreply.github.com>
2023-07-16 20:41:13 -0400

    Update file_read.py

--- bbc/file_read.py
+++ bbc/file_read.py
@@ -4,7 +4,8 @@ import pandas
 from bbc.accounts_receivable import AccountsReceivable, MarginableAR
 
 def read_accounts_receivable(file_name) -> AccountsReceivable:
-        # Read insured AR
+        
+           # Read insured AR
     # To open the workbook
     # workbook object is created
     wb_obj = openpyxl.load_workbook(file_name)
@@ -21,10 +22,8 @@ def read_accounts_receivable(file_name) -> AccountsReceivable:
     #         break
     #         else
     #          return
-
-
-    # Read insured AR
-    file_AR = pandas.read_excel(file_name, sheet_name="Insured AR", skiprows=[0, 1])
+        
+    file_AR = pandas.read_excel(file_name, sheet_name="Insured AR")
 
     file_AR_RN = file_AR.dropna()
     cell_obj_11 = file_AR_RN(row=1, column=1)
@@ -36,7 +35,8 @@ def read_accounts_receivable(file_name) -> AccountsReceivable:
     cell_obj_17 = file_AR_RN(row=1, column=7)
     cell_obj_18 = file_AR_RN(row=1, column=8)
 
-    customer_name = ["Customer", "Customer Name", "Code", "Name", "Client", "Account")
+        # below section needs to be expanded with more hard coded values as we see more specific vlaues from Banks
+    customer_name = ["Customer", "Customer Name", "Code", "Name", "Client", "Account"]
     upto_30Days = ["30 Days","1-30 past due","Current","30","Current - 30 days","1 - 30","0-30","1-30"]
     upto_60Days = ["60 Days","31-60 past due","30-60 past due","31 - 60 past due","30 - 60 past due","60","30 - 60 days","30 - 60","30-60","31-60","31 - 60","Period 1"]
     upto_90Days = ["90 Days","61-90 past due","60-90 past due","61 - 90 past due","60 - 90 past due","90","60 - 90 days","60 - 90","60-90","61-90","61 - 90","Period 2"]
@@ -44,42 +44,39 @@ def read_accounts_receivable(file_name) -> AccountsReceivable:
     Total_AR = ["Balance","Total","Balance Due","Due"]
         
     if cell_obj_12 in above_90Days:
-            AR_above90Days = file_AR["cell_obj_12"].sum()
+    if file_AR_RN(row=max_row, column=2)= file_AR????????
+            Insured_AR_above90Days = file_AR["cell_obj_12"].sum()
     elif cell_obj_13 in above_90Days:
-            AR_above90Days = file_AR["cell_obj_13"].sum() 
+            Insured_AR_above90Days = file_AR["cell_obj_13"].sum() 
     elif cell_obj_14 in above_90Days:
-            AR_above90Days = file_AR["cell_obj_14"].sum() 
+            Insured_AR_above90Days = file_AR["cell_obj_14"].sum() 
     elif cell_obj_15 in above_90Days:
-            AR_above90Days = file_AR["cell_obj_15"].sum() 
+            Insured_AR_above90Days = file_AR["cell_obj_15"].sum() 
     elif cell_obj_16 in above_90Days:
-            AR_above90Days = file_AR["cell_obj_16"].sum() 
+            Insured_AR_above90Days = file_AR["cell_obj_16"].sum() 
     elif cell_obj_17 in above_90Days:
-            AR_above90Days = file_AR["cell_obj_13"].sum() 
+            Insured_AR_above90Days = file_AR["cell_obj_13"].sum() 
     else cell_obj_18 in above_90Days:
-            AR_above90Days = file_AR["cell_obj_13"].sum() 
+            Insured_AR_above90Days = file_AR["cell_obj_13"].sum() 
 
     if cell_obj_12 in Total_AR:
-            AR_Total = file_AR["cell_obj_12"].sum()
+            Insured_AR_Total = file_AR["cell_obj_12"].sum()
     elif cell_obj_13 in Total_AR:
-            AR_Total = file_AR["cell_obj_13"].sum() 
+            Insured_AR_Total = file_AR["cell_obj_13"].sum() 
     elif cell_obj_14 in Total_AR:
-            AR_Total = file_AR["cell_obj_14"].sum() 
+            Insured_AR_Total = file_AR["cell_obj_14"].sum() 
     elif cell_obj_15 in Total_AR:
-            AR_Total = file_AR["cell_obj_15"].sum() 
+            Insured_AR_Total = file_AR["cell_obj_15"].sum() 
     elif cell_obj_16 in Total_AR:
-            AR_Total = file_AR["cell_obj_16"].sum() 
+            Insured_AR_Total = file_AR["cell_obj_16"].sum() 
     elif cell_obj_17 in Total_AR:
-            AR_Total = file_AR["cell_obj_13"].sum() 
+            Insured_AR_Total = file_AR["cell_obj_13"].sum() 
     else cell_obj_18 in Total_AR:
-            AR_Total = file_AR["cell_obj_13"].sum() 
-
-    insured_ar_total = file_AR["Balance"].sum()
-    AR_Greater_Than_90_Days = file_AR["> 90 day"].sum()
+            Insured_AR_Total = file_AR["cell_obj_13"].sum() 
 
     # Read uninsured AR
     file_uninsured_AR = pandas.read_excel(
-        file_name, sheet_name="Uninsured AR", skiprows=[0, 1]
-    )
+        file_name, sheet_name="Uninsured AR")
     uninsured_ar_total = file_uninsured_AR["Balance"].sum()
     uninsured_ar_Greater_Than_90_Days = file_uninsured_AR["> 90 day"].sum()
 
